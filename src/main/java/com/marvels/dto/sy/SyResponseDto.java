c586af5bf473f2158498e866c81ab758f6b6bb73
renyz <renyz@cloudtechcredit.com>
2019-10-11 14:02:25 +0800

    接口请求返回结果公共参数对象

--- src/main/java/com/marvels/dto/sy/SyResponseDto.java
+++ src/main/java/com/marvels/dto/sy/SyResponseDto.java
@@ -1,5 +1,46 @@
 package com.marvels.dto.sy;
 
-public class SyResponseDto {
 
+public class SyResponseDto {
+    /**结果代码（必返回），返回0或者0000表示正常，其他为错误代码**/
+    private String ec;
+    //显示给用户的错误信息
+    private String em;
+    //参见具体交易接口
+    private String body;
+
+    public SyResponseDto(String ec,String em,String body) {
+        this.ec = ec;
+        this.em = em;
+        this.body = body;
+    }
+
+    public SyResponseDto(String ec,String em) {
+        this.ec = ec;
+        this.em = em;
+    }
+
+    public String getEc() {
+        return ec;
+    }
+
+    public void setEc(String ec) {
+        this.ec = ec;
+    }
+
+    public String getEm() {
+        return em;
+    }
+
+    public void setEm(String em) {
+        this.em = em;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
