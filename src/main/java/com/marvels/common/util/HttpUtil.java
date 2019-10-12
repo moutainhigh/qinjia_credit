5c76a433af5e11573c24f2fdd7830aa48e2e1e38
renyz <renyz@cloudtechcredit.com>
2019-10-12 18:11:18 +0800

    提交请求hander写入

--- src/main/java/com/marvels/common/util/HttpUtil.java
+++ src/main/java/com/marvels/common/util/HttpUtil.java
@@ -8,8 +8,25 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 
+/**
+ *
+ * 请求接口公共方法
+ * @author renyz
+ */
 public class HttpUtil {
     static Logger log = Logger.getLogger(HttpUtil.class);
+
+
+    /**获取secret参数**/
+    private static  String secret = PropertiesLoadUtil.getPropertiesValue("qj.jiufu.httpHand.secret", "forms-openapi.properties");
+    /**
+     * 设置hander头
+     */
+    /**商户编码(金融云平台给各商户分配的编码)**/
+    private static  String code = PropertiesLoadUtil.getPropertiesValue("qj.jiufu.httpHand.code", "forms-openapi.properties");
+    /**资金机构编号(本次业务请求对应的资金机构编号)**/
+    private static String orgCode = PropertiesLoadUtil.getPropertiesValue("qj.jiufu.httpHand.orgCode", "forms-openapi.properties");;
+
     /**
      * 向指定URL发送GET方法的请求
      *
@@ -74,6 +91,107 @@ public class HttpUtil {
             connection.disconnect();// 关闭远程连接
         }
 
+        return result;
+    }
+    /**
+     * 向指定 URL 发送POST方法的请求
+     *
+     * @param httpUrl
+     *            发送请求的 URL
+     * @param param
+     *            请求参数应该是{"key":"==g43sEvsUcbcunFv3mHkIzlHO4iiUIT R7WwXuSVKTK0yugJnZSlr6qNbxsL8OqCUAFyCDCoRKQ882m6cTTi0q9uCJsq JJvxS+8mZVRP/7lWfEVt8/N9mKplUA68SWJEPSXyz4MDeFam766KEyvqZ99d"}的形式。
+     * @return 所代表远程资源的响应结果
+     */
+    public static String doPostByJiuFu(String interFaceCode,String httpUrl, String param) {
+        //获取当前时间戳
+        Long reqTime = System.currentTimeMillis();
+        //请求时间戳(请求时间对应的毫秒数)
+        String time = reqTime.toString();
+        //接口编号(每个接口的编号)
+        String intfCode = interFaceCode;
+        /**请求签名(md5(code+time+secret)，其中，secret为金融云平台分配给商户的密钥)*/
+        String sign = EncryptUtil.MD5Encode(code+time+secret);
+
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        OutputStream os = null;
+        BufferedReader br = null;
+        String result = null;
+        try {
+            URL url = new URL(httpUrl);
+            // 通过远程url连接对象打开连接
+            connection = (HttpURLConnection) url.openConnection();
+            // 设置连接请求方式
+            connection.setRequestMethod("POST");
+            // 设置连接主机服务器超时时间：15000毫秒
+            connection.setConnectTimeout(15000);
+            // 设置读取主机服务器返回数据超时时间：60000毫秒
+            connection.setReadTimeout(60000);
+            //设置hander信息
+            connection.setRequestProperty("intfCode",intfCode);
+            connection.setRequestProperty("code",code);
+            connection.setRequestProperty("orgCode",orgCode);
+            connection.setRequestProperty("time",time);
+            connection.setRequestProperty("sign",sign);
+
+            // 默认值为：false，当向远程服务器传送数据/写数据时，需要设置为true
+            connection.setDoOutput(true);
+            // 默认值为：true，当前向远程服务读取数据时，设置为true，该参数可有可无
+            connection.setDoInput(true);
+            // 设置传入参数的格式:请求参数应该是 name1=value1&name2=value2 的形式。
+            connection.setRequestProperty("Content-Type", "application/json");
+            // 设置鉴权信息：Authorization: Bearer da3efcbf-0845-4fe3-8aba-ee040be542c0
+            //connection.setRequestProperty("Authorization", "Bearer da3efcbf-0845-4fe3-8aba-ee040be542c0");
+            // 通过连接对象获取一个输出流
+            os = connection.getOutputStream();
+            // 通过输出流对象将参数写出去/传输出去,它是通过字节数组写出的
+            os.write(param.getBytes());
+            // 通过连接对象获取一个输入流，向远程读取
+            if (connection.getResponseCode() == 200) {
+
+                is = connection.getInputStream();
+                // 对输入流对象进行包装:charset根据工作项目组的要求来设置
+                br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+                StringBuffer sbf = new StringBuffer();
+                String temp = null;
+                // 循环遍历一行一行读取数据
+                while ((temp = br.readLine()) != null) {
+                    sbf.append(temp);
+                    sbf.append("\r\n");
+                }
+                result = sbf.toString();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if (null != br) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != os) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 断开与远程地址url的连接
+            connection.disconnect();
+        }
         return result;
     }
 
