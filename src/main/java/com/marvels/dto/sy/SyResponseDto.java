b17359eab2f9e8c4dbacab2acebf28b7005712c5
houyl@cloudtechcredit.com <qwer6789>
2019-10-12 10:53:15 +0800

    代码调整

--- src/main/java/com/marvels/dto/sy/SyResponseDto.java
+++ src/main/java/com/marvels/dto/sy/SyResponseDto.java
@@ -8,10 +8,14 @@ import lombok.Setter;
 public class SyResponseDto {
     /**结果代码（必返回），返回0或者0000表示正常，其他为错误代码**/
     private String ec;
-    //显示给用户的错误信息
+    /**显示给用户的错误信息 */
     private String em;
-    //参见具体交易接口
+    /**参见具体交易接口*/
     private String body;
+
+    public SyResponseDto() {
+
+    }
 
     public SyResponseDto(String ec,String em,String body) {
         this.ec = ec;
