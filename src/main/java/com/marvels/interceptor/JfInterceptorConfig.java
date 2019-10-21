39ff129da4231d9566099c2af0f3794260e1b0e3
wangyupi <543371378@qq.com>
2019-10-21 15:25:45 +0800

    拦截规则

--- src/main/java/com/marvels/interceptor/JfInterceptorConfig.java
+++ src/main/java/com/marvels/interceptor/JfInterceptorConfig.java
@@ -20,7 +20,7 @@ public class JfInterceptorConfig extends WebMvcConfigurationSupport {
         // 多个拦截器组成一个拦截器链
         // addPathPatterns 用于添加拦截规则，/**表示拦截所有请求
         // excludePathPatterns 用户排除拦截
-        registry.addInterceptor(jfInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(jfInterceptor).addPathPatterns("/**").excludePathPatterns("/xyqb/druid/**");
         super.addInterceptors(registry);
     }
 }
